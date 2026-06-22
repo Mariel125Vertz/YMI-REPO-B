52112bd331cb6b0ed22f99d470e766916eade7ce
Mariel125Vertz <marielvertz@gmail.com>
2026-06-22 09:29:07 -0600

    PRUEBA

--- config/database.php
+++ config/database.php
@@ -1,16 +1,18 @@
 <?php
+// Sustituimos getenv por los valores reales que vimos en tu panel de Railway
+$host = 'mysql.railway.internal';
+$user = 'root';
+$pass = 'iSEJuNYrtspXucReBwyYbNnaZxtcmXQU';
+$db   = 'railway';
+$port = 3306;
 
-$host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
-$db   = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD'); // Aquí guardas la contraseña en $pass
-
-// CAMBIA ESTA LÍNEA:
-$conexion = new mysqli($host, $user, $pass, $db, $port); 
-// Asegúrate de incluir $port, ¡es importante en Railway!
+// Conexión forzada
+$conexion = new mysqli($host, $user, $pass, $db, (int)$port);
 
 if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    // Si sigue fallando, esto nos dirá exactamente qué pasa
+    die("Error de conexión: " . $conexion->connect_error . " | Host usado: " . $host);
+} else {
+    echo "¡Conexión exitosa!";
 }
 ?>
