5184298086016068adc39867604ac04b406504db
Mariel125Vertz <marielvertz@gmail.com>
2026-06-22 18:26:27 -0600

    PRUEBA 03

--- controllers/PagoController.php
+++ controllers/PagoController.php
@@ -30,8 +30,8 @@ class PagoController
                     ]
                 ],
                 "mode" => "payment",
-                "success_url" => "https://ymi-repo-mfqa5go9e-mariel125vertzs-projects.vercel.app/premium?status=success",
-                "cancel_url" => "https://ymi-repo-mfqa5go9e-mariel125vertzs-projects.vercel.app/premium?status=cancel"
+                "success_url" => "https://ymi-repo-f.vercel.app/premium?status=success",
+"cancel_url"  => "https://ymi-repo-f.vercel.app/premium?status=cancel"
             ]);
 
             echo json_encode(["url" => $session->url]);
